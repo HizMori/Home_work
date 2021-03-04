af9f01841808eeac63cadf78c484b579668b846b
Katya <katya.gan@yandex.ru>
2021-03-04 20:27:04 +0500

    Дз по инфе (05.03.21)

--- Dz_05.03.py
+++ Dz_05.03.py
@@ -11,42 +11,31 @@
     print(f"Цифры {x} и {y} встречаются {str(x).count(str(x)) and str(y).count(str(y))} раз")
 
 #zd_24
-    numbers = map(int, input("Введите последовательность чисел: ").split(" "))
-    x = int(input("Введите число x: "))
+numbers = list(map(int, input("Введите последовательность чисел: ").split()))
+x = int(input("Введите число x: "))
 
-    print(f"Сумма всех чисел последовательности больше числа {x}: {sum([i for i in numbers if i > x])}")
-    print(f"Кол-во всех чётных чисел последовательности: {len([i for i in numbers if i % 2 == 0])}")
-    # numbers = map(int, input("Введите последовательность чисел: ").split(" "))
-    # x = int(input("Введите число x: "))
+sum = sum([i for i in numbers if i > x])
+quantity = len([i for i in numbers if i % 2 == 0])
 
-    # print(f"Сумма всех чисел последовательности больше числа {x}: {sum([i for i in numbers if i > x])}")
-    # print(f"Кол-во чётных чисел последовательности: {len([i for i in numbers if i % 2 == 0])}")
-
-    #a = [1, 2, 3, 4, 5]
-    #b = []
-    #for x in a:
-        #if x % 2 == 0:
-            #b.append(x)
+print(f"Сумма всех чисел последовательности больше числа {x}: {sum}")
+print(f"Кол-во всех чётных чисел последовательности: {quantity}")
 
 #zd_26
-    n = int(input("Введите число n: "))
+n = int(input("Введите число n: "))
 
-    print(f"Максимальная цифра числа {n}: {max(str(n))}\nМинимальная цифра числа {n}: {min(str(n))}")
-
-    maxi = max(str(n))
-    mini = min(str(n))
-    print(f"Максимальная цифра числа {n}: {m}\nМинимальная цифра числа {n}: {e})
+maxi = max(str(n))
+mini = min(str(n))
+print(f"Максимальная цифра числа {n}: {m}\nМинимальная цифра числа {n}: {e})
 
 #zd_27
 
-    n = int(input("Введите число n: "))
+n = int(input("Введите число n: "))
 
-    print(f"Максимальная цифра числа {n}: {max(str(n))}\nМинимальная цифра числа {n}: {min(str(n))}")
-    print(f"Максимальное число превышает минимальное число на {int(max(str(n))) - int(min(str(n)))}")
-    print(f"Сумма максимальной и минимальной цифр числа: {int(max(str(n))) + int(min(str(n)))}")
-
-    maxi = max(str(n))
-    mini = min(str(n))
-    difference = int(max(str(n))) - int(min(str(n)))
-    sum = int(max(str(n))) + int(min(str(n)))
+maxi = max(str(n))
+mini = min(str(n))
+difference = int(max(str(n))) - int(min(str(n)))
+sum = int(max(str(n))) + int(min(str(n)))
+print(f"Максимальная цифра числа {n}: {maxi}\nМинимальная цифра числа {n}: {mini}")
+print(f"Максимальное число превышает минимальное число на {difference}")
+print(f"Сумма максимальной и минимальной цифр числа: {sum}")
 
