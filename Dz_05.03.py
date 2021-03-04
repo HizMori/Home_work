1debfe3912a5bb183fd79648ea47d7902332aad5
Katya <katya.gan@yandex.ru>
2021-03-04 20:47:49 +0500

    Дз по инфе (05.03.21)

--- Dz_05.03.py
+++ Dz_05.03.py
@@ -1,20 +1,19 @@
-#zd_26
-n = int(input("Введите число n: "))
+def zd_26():
+    n = int(input("Введите число n: "))
 
-maxi = max(str(n))
-mini = min(str(n))
-print(f"Максимальная цифра числа {n}: {m}\nМинимальная цифра числа {n}: {e}")
+    maxi = max(str(n))
+    mini = min(str(n))
+    print(f"Максимальная цифра числа {n}: {maxi}\nМинимальная цифра числа {n}: {mini}")
 
-#zd_27
+def zd_27():
+    n = int(input("Введите число n: "))
 
-n = int(input("Введите число n: "))
+    maxi = max(str(n))
+    mini = min(str(n))
+    difference = int(max(str(n))) - int(min(str(n)))
+    sum = int(max(str(n))) + int(min(str(n)))
 
-maxi = max(str(n))
-mini = min(str(n))
-difference = int(max(str(n))) - int(min(str(n)))
-sum = int(max(str(n))) + int(min(str(n)))
-
-print(f"Максимальная цифра числа {n}: {maxi}\nМинимальная цифра числа {n}: {mini}")
-print(f"Максимальное число превышает минимальное число на {difference}")
-print(f"Сумма максимальной и минимальной цифр числа: {sum}")
+    print(f"Максимальная цифра числа {n}: {maxi}\nМинимальная цифра числа {n}: {mini}")
+    print(f"Максимальное число превышает минимальное число на {difference}")
+    print(f"Сумма максимальной и минимальной цифр числа: {sum}")
 
