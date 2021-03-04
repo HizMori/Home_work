36299fedc0eff3a49a231c08aca1da7e177bfc6b
Katya <katya.gan@yandex.ru>
2021-03-04 20:22:25 +0500

    Дз по инфе (05.03.21)

--- Dz_05.03.py
+++ Dz_05.03.py
@@ -1,4 +1,4 @@
-def zd_23(n: int, a: int, z: int, x: int, y: int) -> None:
+#zd_23
     n = int(input("Введите число n: "))
     a = int(input("Введите число a: "))
     z = int(input("Введите число z: "))
@@ -10,7 +10,7 @@ def zd_23(n: int, a: int, z: int, x: int, y: int) -> None:
     print(f"Сумма его больших {a}: {len([i for i in str(n) if int(i) > a])}")
     print(f"Цифры {x} и {y} встречаются {str(x).count(str(x)) and str(y).count(str(y))} раз")
 
-def zd_24(nambers: list, x: int) -> None:
+#zd_24
     numbers = map(int, input("Введите последовательность чисел: ").split(" "))
     x = int(input("Введите число x: "))
 
@@ -28,17 +28,25 @@ def zd_24(nambers: list, x: int) -> None:
         #if x % 2 == 0:
             #b.append(x)
 
-def zd_26(n: int) -> None:
+#zd_26
     n = int(input("Введите число n: "))
 
     print(f"Максимальная цифра числа {n}: {max(str(n))}\nМинимальная цифра числа {n}: {min(str(n))}")
-    m = max(str(n))
-    e = min(str(n))
+
+    maxi = max(str(n))
+    mini = min(str(n))
     print(f"Максимальная цифра числа {n}: {m}\nМинимальная цифра числа {n}: {e})
 
-def zd_27(n: int) -> None:
+#zd_27
+
     n = int(input("Введите число n: "))
 
     print(f"Максимальная цифра числа {n}: {max(str(n))}\nМинимальная цифра числа {n}: {min(str(n))}")
     print(f"Максимальное число превышает минимальное число на {int(max(str(n))) - int(min(str(n)))}")
     print(f"Сумма максимальной и минимальной цифр числа: {int(max(str(n))) + int(min(str(n)))}")
+
+    maxi = max(str(n))
+    mini = min(str(n))
+    difference = int(max(str(n))) - int(min(str(n)))
+    sum = int(max(str(n))) + int(min(str(n)))
+
