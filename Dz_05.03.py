44ccbb7113f84341e8d5e1987a55e3d1614341bf
Katya <katya.gan@yandex.ru>
2021-03-04 20:45:38 +0500

    Дз по инфе (05.03.21)

--- Dz_05.03.py
+++ Dz_05.03.py
@@ -1,19 +1,10 @@
-#zd_23
-    n = int(input("Введите число n: "))
-    a = int(input("Введите число a: "))
-    z = int(input("Введите число z: "))
-    x = int(input("Введите число x: "))
-    y = int(input("Введите число y: "))
-
-    print(f"В числе {n} встречаетсься цифра {a} {str(a).count(str(a))} раз")
-    print(f"Цифр в числе {n} кратных {z}: {len([i for i in str(n) if int(i) % z == 0])}")
-    print(f"Сумма его больших {a}: {len([i for i in str(n) if int(i) > a])}")
-    print(f"Цифры {x} и {y} встречаются {str(x).count(str(x)) and str(y).count(str(y))} раз")
-
 #zd_24
 numbers = list(map(int, input("Введите последовательность чисел: ").split()))
 x = int(input("Введите число x: "))
 
+for i in numbers
+    if i > x:
+        print(f"Сумма всех чисел последовательности больше числа {x}: {sum}")
 sum = sum([i for i in numbers if i > x])
 quantity = len([i for i in numbers if i % 2 == 0])
 
@@ -25,7 +16,7 @@ n = int(input("Введите число n: "))
 
 maxi = max(str(n))
 mini = min(str(n))
-print(f"Максимальная цифра числа {n}: {m}\nМинимальная цифра числа {n}: {e})
+print(f"Максимальная цифра числа {n}: {m}\nМинимальная цифра числа {n}: {e}")
 
 #zd_27
 
@@ -35,6 +26,7 @@ maxi = max(str(n))
 mini = min(str(n))
 difference = int(max(str(n))) - int(min(str(n)))
 sum = int(max(str(n))) + int(min(str(n)))
+
 print(f"Максимальная цифра числа {n}: {maxi}\nМинимальная цифра числа {n}: {mini}")
 print(f"Максимальное число превышает минимальное число на {difference}")
 print(f"Сумма максимальной и минимальной цифр числа: {sum}")
