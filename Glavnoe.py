e80c7bbb726a0d0bb575d5daad2753e46374ed48
Katya <katya.gan@yandex.ru>
2021-03-28 17:54:19 +0500

    Подготовка

--- Glavnoe.py
+++ Glavnoe.py
@@ -16,3 +16,21 @@ print(f'Проценты: {12345:.3%}')
 number = .12345
 print("{:.2%}".format(number))
 
+a, b = map(int, input('Ввод нескольких переменных в сторку: ').split())
+print(a, b)
+
+print('import math - подключение математического модуля')
+
+import math
+print(f'Число «пи»: {math.pi}')
+print(f'Квадратный корень: {math.sqrt(4)}')
+print(f'Синус угла, заданного в радианах: {math.sin(45)}')
+print(f'Косинус угла, заданного в радианах: {math.cos(45)}')
+print(f'Округление «вниз»: {math.floor(15.6)}')
+print(f'Округление «вверх»: {math.ceil(15.4)}')
+
+import random
+
+print(f'Целые числа на отрезке [a,b]: {random.randint(1,5)}')
+print(f'Генератор на [0,1): {random.random()}')
+print(f'Генератор на [a, b] (вещественные числа): {random.uniform(1.5, 10.5)}')
