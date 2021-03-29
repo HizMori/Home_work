f8bcff4b7bb317008bd637bd66454ad66ef2620b
Katya <katya.gan@yandex.ru>
2021-03-29 18:19:44 +0500

    Подготовка

--- Dla ege.py
+++ Dla ege.py
@@ -8,15 +8,13 @@
 сначала количество, затем максимальное число.
 '''
 
-n = 5883
 k = 0
 m = -1
-while n != 15906:
-    if n % 9 == 0 or n % 23 == 0:
-        if n % 13 != 0 and n % 18 != 0 and n % 19 != 0 and n % 22 != 0:
+for i in range(5883, 15907):
+    if i % 9 == 0 or i % 23 == 0:
+        if i % 13 != 0 and i % 18 != 0 and i % 19 != 0 and i % 22 != 0:
             k += 1
-            m = max(m, n)
-    n += 1
+            m = max(m, i)
 print(k, m)
 
 '''
@@ -26,15 +24,13 @@ print(k, m)
 сначала количество, затем наименьшее число.
 '''
 
-n = 2*10**5
 k = 0
 m = 1000000
-while n != 4*10**5:
-    if n % 7 == 0:
-            if n % 13 != 0 and n % 29 != 0 and n % 43 != 0 and n % 101 != 0:
+for i in range(2*10**5, 4*10**5+1):
+    if i % 7 == 0:
+            if i % 13 != 0 and i % 29 != 0 and i % 43 != 0 and i % 101 != 0:
                 k += 1
-                m = min(m, n)
-    n += 1
+                m = min(m, i)
 print(k, m) #незабыть 5 нулей на конце
 
 '''
@@ -50,25 +46,12 @@ def sumDigits(e):
         e = e // 10
     return sumi
 
-
-n = 9999
 k = 0
 m = -1
-while n != 99999:
-    if n % sumDigits(n) == 0:
-        k += 1
-        m = max(m, n)
-    n += 1
-print(k, m)
-
-#или
-
-k = 0
-m = 0
-for i in range(9999, 99999 + 1):
+for i in range(9999, 100000):
     if i % sumDigits(i) == 0:
-        k = k + 1
-        m = i
+        k += 1
+        m = max(m, i)
 print(k, m)
 
 '''
