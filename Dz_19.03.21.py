38e75e69b2b9b7d2d5524c666226214b7d2709f4
Katyusha <74827754+HizMori@users.noreply.github.com>
2021-04-15 19:55:11 +0500

    dz

--- Dz_19.03.21.py
+++ Dz_19.03.21.py
@@ -47,21 +47,50 @@ print(m)
 '''
 #/////
 from random import  randint
-a, b = map(int, input('Введите границы диапазона: ').split())
+
+print('Введите границы диапазона:')
+a, b = map(int, input().split())
 A = [randint(a, b) for i in range(10)]
 print(*A)
 
 #////
-a, b = sorted(int(i) for i in input('Введите границы диапазона: ').split())
+from random import  randint
+
+print('Введите границы диапазона:')
+a, b = sorted(int(i) for i in input().split())
 A = [randint(a, b) for i in range(10)]
 
 print(*A)
 
 #////
-a, b = sorted(int(i) for i in input('Введите границы диапазона: ').split())
+from random import  randint
+
+print('Введите границы диапазона:')
+a, b = sorted(int(i) for i in input().split())
 A = []
 
 for i in range(10):
     A.append(randint(a, b) if i < 5
-             else pow(A[1 - 5], 2))
+             else pow(A[i - 5], 2))
 print(*A)
+
+#///
+print('Массив:')
+A = [int(i) for i in input().split()]
+print(f'Среднее арифметическое {sum(A)/len(A)}')
+
+#///
+from random import  randint
+
+print('Массив:')
+A = [randint(0, 101) for i in range(10)]
+for a in A:
+  print (a, end = " ")
+
+for i in A:
+    if i < 50:
+        a = sum(A) / len(A)
+        A.remove(i)
+    else:
+        b = sum(A) / len(A)
+print(f'\nСр. арифм. элементов < 50: {a}\nСр. арифм. элементов >=50: {b}')
