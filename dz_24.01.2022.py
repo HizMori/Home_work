6157eee7cfaf01feab3fa76ea717968cc54520fb
Katyusha <74827754+HizMori@users.noreply.github.com>
2022-01-25 14:48:52 +0500

    dz

--- dz_24.01.2022.py
+++ dz_24.01.2022.py
@@ -8,3 +8,13 @@ for n in range(1, 70):
     if r > 56:
         print(r)
         break
+
+#6
+for i in range(70, 1000):
+    s = i
+    n = 1
+    while s*n < 4096:
+        s = s // 2
+        n = n * 4
+    if n == 1024:
+        print(i)
