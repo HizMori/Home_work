fcbdff7ac245f724b9daf090845b22e75ec0d7f4
Katyusha <74827754+HizMori@users.noreply.github.com>
2022-01-25 15:25:57 +0500

    dz

--- dz_24.01.2022.py
+++ dz_24.01.2022.py
@@ -52,7 +52,7 @@ def f(n):
     if n == 1:
         return 1
     if n % 2 == 0:
-        return  n + 2 * f(n - 1)
+        return n + 2 * f(n - 1)
     if n % 2 != 0 and n > 1:
         return 1 + 3 * f(n - 2)
 
