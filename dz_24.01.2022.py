a2d21b6e1e54299c4324c3bf7c9a21a28f83c127
Katyusha <74827754+HizMori@users.noreply.github.com>
2022-01-25 15:10:36 +0500

    dz

--- dz_24.01.2022.py
+++ dz_24.01.2022.py
@@ -18,3 +18,12 @@ for i in range(70, 1000):
         n = n * 4
     if n == 1024:
         print(i)
+
+#12
+a = '1' * 70
+while '1111' in a or '2222' in a:
+    if '1111' in a:
+        a = a.replace('1111', '22', 1)
+    if '2222' in a:
+        a = a.replace('2222', '11', 1)
+print(a)
